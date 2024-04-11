10e51c3e7237231f1fc0a9053f4a6f68eb6a773a
marcyroz <marcelly.farias@fatec.sp.gov.br>
2024-04-11 15:06:56 -0300

    estilização da header e funcionalidade de links

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,4 +1,37 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const Container = styled.div`
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+// quando vamos utilizar um componente que não é nativo do html colocamos ele entre parênteses
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  //faz a opacidade demorar o.2 segundos para mudar
+  transition: 0.2s;
+
+  /* adicionando hover para que o carrinho tenha um efeito visual quando o usuário passar o mouse por cima */
+  &:hover {
+    opacity: 0.7;
+  }
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #999;
+    }
+  }
 `;
