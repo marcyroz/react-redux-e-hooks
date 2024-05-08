f5099391613be927b2e8c629fe79a03a255ba8f4
marcyroz <marcelly.farias@fatec.sp.gov.br>
2024-05-08 13:27:51 -0300

    buscando os produtos na api, corrigindo a baseURL em services/api e formatando o preço dos produtos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,110 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProductList } from './styles';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const fetchHomeData = async () => {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchHomeData();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-          
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img
+            src={product.image}
+            alt={product.title}
+            width={300}
+            height={200}
+          />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button type="button">
+            {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" />3
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src=" https://i0.wp.com/umaturistanasnuvens.com/wp-content/uploads/2020/10/doces-ao-redor-do-mundo-macarons.jpg?resize=1170%2C780&ssl=1"
-          alt="macarons"
-        />
-        <strong>Macarons deliciosos!</strong>
-        <span>R$5,00</span>
-        <button type="button">
-          {/* div para fazer a parte do botão que contem o carrinho de compras e a quantidade de itens */}
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:3333',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
 
 //comando para rodar a api:
-// json-server server.json -p 3333 -w
+// yarn json-server --watch -p 3333 server.json
